664f7494345fa4230ace1a69fd973049b8523bc7
dmitri <deemok@gmail.com>
2020-02-20 16:02:05 +0100

    Bump e to include SELinux bootstrap install phase.
    Fix SELinux detection and automatically turn it off on distributions
    that we do not support SELinux on.
    Also, thread SELinux configuration in upgrades properly.

--- lib/update/system/system.go
+++ lib/update/system/system.go
@@ -179,6 +179,8 @@ type Config struct {
 	Packages update.LocalPackageService
 	// FieldLogger specifies the logger
 	logrus.FieldLogger
+	// SELinux controls SELinux support
+	SELinux bool
 }
 
 func (r *PackageUpdates) checkAndSetDefaults() error {
@@ -232,6 +234,7 @@ func (r *System) applyUpdates(ctx context.Context, updates []storage.PackageUpda
 	packageUpdater := &PackageUpdater{
 		Logger:   log.New(r.WithField(trace.Component, "update:package")),
 		Packages: r.Packages,
+		SELinux:  r.SELinux,
 	}
 	for _, u := range updates {
 		r.WithField("update", u).Info("Applying.")
@@ -692,7 +695,7 @@ func uninstallPackage(
 		logger.WithField("service", servicePackage).Info("Package installed as a service, will uninstall.")
 		err = services.UninstallPackageService(servicePackage)
 		if err != nil {
-			return nil, utils.NewUninstallServiceError(servicePackage)
+			return nil, utils.NewUninstallServiceError(err, servicePackage)
 		}
 	}
 	updates = append(updates, pack.LabelUpdate{
