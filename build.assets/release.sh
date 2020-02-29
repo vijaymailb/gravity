638dee8f297a1512a46ac94b6fc7e5013ea03515
Walt Della <walt@gravitational.com>
2020-02-28 16:33:33 -0800

    Add dependencies to the release target.
    Previously, running make release without running other make targets
    first resulted in a broken build, because dependencies are not properly
    declared.  Now all dependencies are declared, but unfortunately some of
    them are unresolveable.  I hope to address those shortly.

--- build.assets/release.sh
+++ build.assets/release.sh
@@ -5,6 +5,9 @@ TEMP_DIR="$(mktemp -d)"
 trap "rm -rf ${TEMP_DIR}" exit
 
 # Add assets to the release tarball
+#
+# Any content added here should also be added in the make release
+# target dependencies
 cp ${TSH_OUT} ${TELE_OUT} install.sh ../LICENSE ${TEMP_DIR}
 cp release-tarball-README.md ${TEMP_DIR}/README.md
 ../version.sh > ${TEMP_DIR}/VERSION
